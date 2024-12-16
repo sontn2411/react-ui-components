8e8a2e054414e00cc65d8c72a82f5e4f4e96da17
sontn2411 <sontn2411@gmail.com>
2024-12-16 20:43:44 +0700

    update

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
-
-import './button.css';
+import './button.css'
 
 export interface ButtonProps {
   /** Is this the principal call to action on the page? */
-  primary?: boolean;
+  primary?: boolean
   /** What background color to use */
-  backgroundColor?: string;
+  backgroundColor?: string
   /** How large should the button be? */
-  size?: 'small' | 'medium' | 'large';
+  size?: 'small' | 'medium' | 'large'
   /** Button contents */
-  label: string;
+  label: string
   /** Optional click handler */
-  onClick?: () => void;
+  onClick?: () => void
+  radius?: string
 }
 
 /** Primary UI component for user interaction */
@@ -21,17 +20,23 @@ export const Button = ({
   size = 'medium',
   backgroundColor,
   label,
+  radius,
   ...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode = primary
+    ? 'storybook-button--primary'
+    : 'storybook-button--secondary'
   return (
     <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      type='button'
+      className={['storybook-button', `storybook-button--${size}`, mode].join(
+        ' '
+      )}
       style={{ backgroundColor }}
       {...props}
     >
       {label}
+      {radius}
     </button>
-  );
-};
+  )
+}
